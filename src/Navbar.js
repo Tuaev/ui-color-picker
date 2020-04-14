a4cc79e28af299cf609f2cc61d98cbaee8294333
Soslan <sparksoslan@gmail.com>
2020-04-14 17:43:58 +0100

    Add Palette list and links to homepage

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
-
+import { Link } from 'react-router-dom';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
@@ -30,7 +30,7 @@ export default class Navbar extends Component {
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href="/">Color Picker</a>
+          <Link to="/">Color Picker</Link>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>
---
+++ src/PaletteList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class PaletteList extends Component {
+  render() {
+    const { palettes } = this.props;
+    return (
+      <div className="Palette-List">
+        {palettes.map((palette) => (
+          <p>
+            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+          </p>
+        ))}
+      </div>
+    );
+  }
+}
+export default PaletteList;
