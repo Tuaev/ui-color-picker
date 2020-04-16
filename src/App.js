a4d609fbeb676ee4028db1fe32a949ec71d8ad39
Soslan <sparksoslan@gmail.com>
2020-04-16 18:23:57 +0100

    Add SingleColorPalette and Link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
+import SingleColorPalette from './SingleColorPalette';
 import seedColors from './seedColors';
 
 import { generatePalette } from './colorHelpers.js';
@@ -28,11 +29,7 @@ class App extends Component {
             <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
           )}
         />
-        <Route
-          exact
-          path="/palette/:paletteId/:colorId"
-          render={() => <h1>Single color page</h1>}
-        />
+        <Route exact path="/palette/:paletteId/:colorId" render={() => <SingleColorPalette />} />
       </Switch>
       // <div>
       //   <Palette palette={generatePalette(seedColors[4])} />
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -13,7 +13,7 @@ export default class ColorBox extends Component {
   };
 
   render() {
-    const { name, background } = this.props;
+    const { name, background, moreUrl } = this.props;
     const { copied } = this.state;
     return (
       // Displays color and copys to clipboard on click.
@@ -30,7 +30,7 @@ export default class ColorBox extends Component {
             </div>
             <button className="copy-button">Copy</button>
           </div>
-          <Link to="/" onClick={(e) => e.stopPropagation()}>
+          <Link to={moreUrl} onClick={(e) => e.stopPropagation()}>
             <span className="see-more">More</span>
           </Link>
         </div>
