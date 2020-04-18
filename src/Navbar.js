9d2cb30035fd8dcc275dd61cc42fa6fd5a033a61
Soslan <sparksoslan@gmail.com>
2020-04-18 17:47:52 +0100

    Refactor NavBar styles and remove CSS files

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,10 +4,12 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Snackbar from '@material-ui/core/Snackbar';
 import CloseIcon from '@material-ui/icons/Close';
 import IconButton from '@material-ui/core/IconButton';
-import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import Slider from 'rc-slider';
+import { Link } from 'react-router-dom';
+
+import styles from './styles/NavBarStyles';
 import 'rc-slider/assets/index.css';
-import './Navbar.css';
 
 class Navbar extends Component {
   state = {
@@ -25,23 +27,23 @@ class Navbar extends Component {
   };
 
   render() {
-    const { level, changeLevel, showingAllColors } = this.props;
+    const { level, changeLevel, showingAllColors, classes } = this.props;
 
     const { format } = this.state;
     return (
-      <header className="Navbar">
-        <div className="logo">
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
           <Link to="/">Color Picker</Link>
         </div>
         {showingAllColors && (
-          <div className="slider-container">
+          <div>
             <span>Level: {level}</span>
-            <div className="slider">
+            <div className={classes.slider}>
               <Slider defaultValue={level} min={100} max={900} onChange={changeLevel} step={100} />
             </div>
           </div>
         )}
-        <div className="select-container">
+        <div className={classes.selectContainer}>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
@@ -66,4 +68,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
