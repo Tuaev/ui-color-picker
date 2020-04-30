da49b6f9092303f60ad1abad604aa5e81f0cc247
Soslan <sparksoslan@gmail.com>
2020-04-30 22:35:31 +0100

    Reponsive Color Box

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -15,15 +15,15 @@ export default {
     },
     [sizes.down('lg')]: {
       width: '25%',
-      height: (props) => (props.showingFullPalette ? '20%' : '50%'),
+      height: (props) => (props.showingFullPalette ? '20%' : '33.3333%'),
     },
     [sizes.down('md')]: {
       width: '50%',
-      height: (props) => (props.showingFullPalette ? '10%' : '50%'),
+      height: (props) => (props.showingFullPalette ? '10%' : '20%'),
     },
     [sizes.down('xs')]: {
       width: '100%',
-      height: (props) => (props.showingFullPalette ? '5%' : '50%'),
+      height: (props) => (props.showingFullPalette ? '5%' : '10%'),
     },
   },
   copyText: {
@@ -114,6 +114,9 @@ export default {
       marginBottom: '0',
       padding: '1rem',
       textTransform: 'uppercase',
+      [sizes.down('xs')]: {
+        fontSize: '5rem',
+      },
     },
     '& p': {
       fontSize: '2rem',
